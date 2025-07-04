1d9c09493f1d0b1b852af8e5f59b65895168a141
antonioconac <conacheantonio13@gmail.com>
2025-07-04 18:41:07 +0300

    Added new functionality 'Complete' and 'Exit'

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ addButton = fs.Button("Add")
 listBox = fs.Listbox(values=functions.get_todos(), key="todos",
                      enable_events=True, size=[45, 10])
 editButton = fs.Button("Edit")
+completeButton = fs.Button("Complete")
+exitButton = fs.Button("Exit")
 
 window = fs.Window("My to-do app GUI",
-                   layout=[[label], [inputBox, addButton], [listBox, editButton]],
+                   layout=[[label],
+                           [inputBox, addButton],
+                           [listBox, editButton, completeButton],
+                           [exitButton]],
                    font=("Helvetica", 20))
 
 while True:
@@ -33,6 +38,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case 'Exit':
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case fs.WIN_CLOSED:
